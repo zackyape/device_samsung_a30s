a1990ecc3b63184a9c1779141bb99745a851cdbd
zackyape <auahbodoamat123@gmail.com>
2025-12-07 21:04:39 +0700

    Add Banana

---
+++ banana_a30s.mk
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/a30s/aosp_a30s.mk)
+
+# Inherit some common BananaDroid stuff.
+$(call inherit-product, vendor/banana/config/common.mk)
+
+PRODUCT_NAME := banana_a30s
+PRODUCT_DEVICE := a30s
+PRODUCT_BRAND := Samsung
+PRODUCT_MODEL := SM-A307F
+PRODUCT_MANUFACTURER := Samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+# Banana Ify
+BANANA_MAINTAINER := Zsheesh
+BANANA_BUILD_TYPE := UNOFFICIAL
+
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Blur
+TARGET_ENABLE_BLUR := true
+
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Quick Tap
+TARGET_SUPPORTS_QUICK_TAP := true
--- lineage_a30s.mk
+++ lineage_a30s.mk
@@ -1,14 +1,8 @@
 # Copyright (C) 2025 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
-
 # Inherit device configuration
-$(call inherit-product, device/samsung/a30s/device.mk)
+$(call inherit-product, device/samsung/a30s/aosp_a30s.mk)
 
 # Inherit LineageOS common device config
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
